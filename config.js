cb9c0861a22aa75f687c6a1205d2a353d39258bd
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-04-26 09:09:43 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+global.prefix = '-'
+global.botName = '𝐈𝐧𝐤𝐲𝐁𝐨𝐭-𝐌𝐃'
+global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
+
+global.staff = ['50768666666']
+global.owner = ['595995660558', '5492612301593', '522213483793']
+
+global.mess = {
+	wait: 'Por favor espere...'
+}
