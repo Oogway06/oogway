bb487c81335542996cdc29b47ba212c89e0d13ad
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-04-26 09:48:37 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 global.prefix = '-'
 global.botName = '𝐈𝐧𝐤𝐲𝐁𝐨𝐭-𝐌𝐃'
 global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
+global.groupSupport = 'https://chat.whatsapp.com/L5KgudBpBA21mA9ude4cVG'
 
 global.staff = ['50768666666']
 global.owner = ['595995660558', '5492612301593', '522213483793']
