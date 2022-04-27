28ef009df43856579af7664772aa99960bd36ec8
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-04-27 11:33:46 -0400

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -33,6 +33,7 @@ module.exports = async(inky, v, store) => {
 		const q = args.join(' ')
 		const senderNumber = v.sender.split('@')[0]
 		const botNumber = inky.user.id.split(':')[0]
+		const bio = try { (await inky.fetchStatus(v.sender)).status } catch { 'Sin Bio' }
 		
 		const groupMetadata = v.isGroup ? await inky.groupMetadata(v.chat) : ''
 		const groupMembers = v.isGroup ? groupMetadata.participants : ''
