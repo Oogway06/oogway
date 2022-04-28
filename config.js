8959a63678a377d8dcdf0832660679258d619ab9
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-04-28 09:14:00 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,12 @@ global.botName = '𝐈𝐧𝐤𝐲𝐁𝐨𝐭-𝐌𝐃'
 global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
 global.groupSupport = 'https://chat.whatsapp.com/L5KgudBpBA21mA9ude4cVG'
 
-global.staff = ['50768666666']
+global.staff = [
+	'50768666666', // _`Kevin
+	'595994230885', // ℳℛ. ᎶᎾℒⅅ
+	'595985902159', // Álvaro
+	'595993517816' // Ruben P
+]
 global.owner = ['595995660558', '5492612301593', '522213483793']
 
 global.mess = {
