19b3d1ca994837288d7603226bdac9ef256ba708
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-05-03 12:08:30 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,18 +4,16 @@ global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
 global.groupSupport = 'https://chat.whatsapp.com/L5KgudBpBA21mA9ude4cVG'
 
 global.staff = [
-	'50768666666', // _`Kevin
-	'595994230885', // ℳℛ. ᎶᎾℒⅅ
-	'595985902159', // Álvaro
-	'595993517816' // Ruben P
+	'50768666666' // _`Kevin
 ]
 global.owner = ['595995660558', '5492612301593', '522213483793']
 
 global.mess = {
 	wait: 'Por favor espere...',
 	only: {
-		group: 'Comando solo para grupos',
 		admins: 'Necesitas ser admin para ejecutar este comando',
 		botadmin: botName + ' necesita ser admin',
+		group: 'Comando solo para grupos',
+		vip: 'Comando exclusivo para usuarios *✨ Vip ✨*'
 	}
 }
