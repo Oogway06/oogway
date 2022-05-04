e9fcc94b3d775a412ac1870f0d49d30ff7024322
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-05-04 11:22:48 -0400

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -5,6 +5,7 @@ require('../config')
 */
 
 const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, makeInMemoryStore, getContentType } = require('@adiwajshing/baileys')
+const P = require('pino')
 const { exec } = require('child_process')
 const fs = require('fs')
 const hx = require('hxz-api')
