00d4833edeeb82b7c373e19ea04104463bda68a7
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-05-06 12:03:33 -0400

    Update upsert.js

--- message/upsert.js
+++ message/upsert.js
@@ -868,7 +868,7 @@ break
 	} catch (e) {
 		const isError = String(e)
 		
-		inky.sendMessage(v.key.remoteJid, { text: isError }, { quoted: v })
+		inky.sendMessage(v.key.remoteJid, { text: e }, { quoted: v })
 		console.log(e)
 	}
 }
