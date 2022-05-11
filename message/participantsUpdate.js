fa8e266bd82027f732c0beb3d839ceffb3dd9dc4
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-05-11 17:15:00 -0400

    Update participantsUpdate.js

--- message/participantsUpdate.js
+++ message/participantsUpdate.js
@@ -1,1 +1,37 @@
+require('../config')
 
+const { getBuffer } = require('../lib/functions')
+
+module.exports = async(inky, v) => {
+  try {
+		const groupMetadata = await inky.groupMetadata(v.id)
+		const participants = v.participants
+		
+		for (let num of participants) {
+			if (v.action == 'add') {
+				var teks = `\t\t\t\tBienvenido @${num.split('@')[0]}\n\n➼ *Grupo:* ${groupMetadata.subject}\n\n➼ *Descripcion:*\n${groupMetadata.desc}`
+				try {
+					ppimg = await inky.profilePictureUrl(num, 'image')
+				} catch {
+					ppimg = 'https://images4.alphacoders.com/921/921653.png'
+				}
+				var buffer = await getBuffer(ppimg)
+				var buttonMessage = {
+					location: {
+            jpegThumbnail: buffer
+          },
+          caption: teks,
+          footerText: fake,
+          buttons: [
+            { buttonId: prefix + 'menu', buttonText: { displayText: '📜 Menu 📜' }, type: 1 }
+          ],
+          headerType: 6,
+          mentions: [num]
+				}
+				inky.sendMessage(v.id, buttonMessage)
+			}
+		}
+	} catch(e) {
+		console.log(e)
+	}
+}
