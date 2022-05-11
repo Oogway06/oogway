c350b8994abdb72cd757281bb677b157eff99885
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-05-11 17:20:40 -0400

    Update participantsUpdate.js

--- message/participantsUpdate.js
+++ message/participantsUpdate.js
@@ -3,13 +3,13 @@ require('../config')
 const { getBuffer } = require('../lib/functions')
 
 module.exports = async(inky, v) => {
-  try {
+	try {
 		const groupMetadata = await inky.groupMetadata(v.id)
 		const participants = v.participants
-		
 		for (let num of participants) {
+			console.log(v)/*
 			if (v.action == 'add') {
-				var teks = `\t\t\t\tBienvenido @${num.split('@')[0]}\n\n➼ *Grupo:* ${groupMetadata.subject}\n\n➼ *Descripcion:*\n${groupMetadata.desc}`
+				var teks = `\t\t\t\t*Bienvenido @${num.split('@')[0]}*\n\n➼ *Grupo:* ${groupMetadata.subject}\n\n➼ *Descripcion:*\n${groupMetadata.desc}`
 				try {
 					ppimg = await inky.profilePictureUrl(num, 'image')
 				} catch {
@@ -18,18 +18,19 @@ module.exports = async(inky, v) => {
 				var buffer = await getBuffer(ppimg)
 				var buttonMessage = {
 					location: {
-            jpegThumbnail: buffer
-          },
-          caption: teks,
-          footerText: fake,
-          buttons: [
-            { buttonId: prefix + 'menu', buttonText: { displayText: '📜 Menu 📜' }, type: 1 }
-          ],
-          headerType: 6,
-          mentions: [num]
+						jpegThumbnail: buffer
+					},
+					caption: teks,
+					footerText: fake,
+					buttons: [
+						{ buttonId: prefix + 'menu', buttonText: { displayText: '📜 Menu 📜' }, type: 1 },
+						{ buttonId: prefix + 'creador', buttonText: { displayText: '👑 Creador 👑' }, type: 1 }
+					],
+					headerType: 6,
+					mentions: [num]
 				}
 				inky.sendMessage(v.id, buttonMessage)
-			}
+			}*/
 		}
 	} catch(e) {
 		console.log(e)
