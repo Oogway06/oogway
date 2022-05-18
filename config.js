562d69ccccfa0541b375e70fc57b5e5c9a06ec61
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-05-18 19:03:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,9 @@ global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
 global.groupSupport = 'https://chat.whatsapp.com/L5KgudBpBA21mA9ude4cVG'
 
 global.staff = [
-	'50768666666' // _`Kevin
+	'50768666666', // _`Kevin
+	'595976905967', // Cherlo
+	'595986460945' // Aiden
 ]
 global.owner = ['595995660558', '5492612301593', '522213483793']
 
