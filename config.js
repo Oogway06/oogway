4a56c3838bce6c2c564c6d1bcd74856172c1ef98
InkyGod03 <95667342+InkyGod03@users.noreply.github.com>
2022-06-02 17:11:23 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ global.fake = botName + ' ➣ ©ｴɳƙყᴳᵒᵈ'
 global.groupSupport = 'https://chat.whatsapp.com/L5KgudBpBA21mA9ude4cVG'
 
 global.staff = [
-	'5492612301593', // Inky +54
 	'595976905967', // Cherlo
 	'595986460945' // Aiden
 ]
 global.owner = [
 	'595995660558', // Inky +595
+	'5492612301593', // Inky +54
 	'50768666666' // _`Kevin
 ]
 
